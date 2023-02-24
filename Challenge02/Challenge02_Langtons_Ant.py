4be1f9cc7a1d91e36183115a6b90ed5fb386a6ce
Phil Palmer <philip.palmer@and.digital>
2023-02-24 15:37:07 +0000

    update

--- Challenge02/Challenge02_Langtons_Ant.py
+++ Challenge02/Challenge02_Langtons_Ant.py
@@ -7,20 +7,45 @@ Coding Challenge: Langton's Ant
 __author__ = 'Phil Palmer'
 __version__ = '0.0.1'
 
-import turtle
+import turtle as t
 
-screen = turtle.Screen()
-screen.bgcolor('yellow')
-screen.screensize(10,10)
+def adam_loc(adam):
+    """Docstring: function to return x,y coordinates of Adam"""
+    return (round(adam.xcor()), round(adam.ycor()))
 
-loc_col_map = {}
+screen = t.Screen()
+screen.bgcolor('white')
+screen.screensize(500,500)
 
-adam = turtle.Turtle()
+stamp_color_map = {}
+
+adam = t.Turtle()
 adam.shape('square')
 adam.shapesize(1)
-adam.speed(100)
-turtle.Screen().exitonclick()
+adam.speed(10)
+distance = 20
 
-step = 20
-adam.forward(step)
-adam.right(90)
+loc = adam_loc(adam)
+
+while True:
+
+    if loc not in stamp_color_map or stamp_color_map[loc] == "white":
+
+        adam.fillcolor("black")
+        adam.stamp()
+        stamp_color_map[adam_loc(adam)] = "black"
+        adam.rt(90)
+        adam.fd(distance)
+        loc = adam_loc(adam)
+        t.Screen().exitonclick()
+
+    elif stamp_color_map[loc] == "black":
+
+        adam.fillcolor("white")
+        adam.stamp()
+        stamp_color_map[adam_loc(adam)] = "white"
+        adam.lt(90)
+        adam.fd(distance)
+        loc = adam_loc(adam)
+
+        t.Screen().exitonclick()
